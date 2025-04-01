5094203ea5bb421773994f3653f42e3e6c575117
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-04-01 14:47:46 -0700

    Implement Wave_3 and pass tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,11 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        self.id = uuid.uuid1().int if id is None else id
+    
+    def get_category(self):
+        return "Item"
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,6 @@
 class Vendor:
-    def __init__(self, inventory=[]):
-        self.inventory = inventory
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
 
     def add(self, item):
         self.inventory.append(item)
@@ -12,3 +12,25 @@ class Vendor:
             return item
         except ValueError:
             return False
+
+    def get_by_id(self, id):
+
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
+
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if not self.remove(my_item):
+            return False
+
+        if not other_vendor.remove(their_item):
+            self.add(my_item)
+            return False
+        
+        other_vendor.add(my_item)
+        self.add(their_item)
+
+        return True
+
