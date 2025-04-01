29e4e7c48e774a5cc755613de7a31117d77608cd
Natalia Razmadze <natasha.razmadze@gmail.com>
2025-04-01 15:27:18 -0700

    Wave 4 was implemented

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -34,3 +34,11 @@ class Vendor:
 
         return True
 
+    def swap_first_item(self, other_vendor):
+        if not self.inventory or not other_vendor.inventory:
+            return False
+
+        my_first_item = self.inventory[0]
+        their_first_item = other_vendor.inventory[0]
+
+        return self.swap_items(other_vendor, my_first_item, their_first_item)
