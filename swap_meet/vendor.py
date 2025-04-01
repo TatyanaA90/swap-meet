666621cf6fee087dc72ead38e96268aa4cb62f5a
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-04-01 14:55:01 -0700

    Fix bugs after merge conflict

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,6 @@
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = inventory if inventory is not None else []
-    def __init__(self, inventory=None):
-        self.inventory = inventory if inventory is not None else []
 
     def add(self, item):
         self.inventory.append(item)
