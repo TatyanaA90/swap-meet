11f0419f525a4e97fe2d70ec108985c03a288bda
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-04-01 17:31:36 -0700

    Implement Wave_5 for Clothing class and pass tests

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,11 @@
+import uuid
 class Clothing:
-    pass
+    def __init__(self, id=None, fabric="Unknown"):
+        self.id = uuid.uuid1().int if id is None else id
+        self.fabric = fabric
+    
+    def get_category(self):
+        return "Clothing"
+
+    def __str__(self): 
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
